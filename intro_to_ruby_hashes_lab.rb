78b1ca46f4ba726005a3cfa032786695a2bc8d0b
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 18:45:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,19 +3,19 @@ def new_hash
 end
 
 def my_hash
-  my_hash = { :name => "Mike" }
+  my_hash = {:name => "Mike"}
 end
 
 def pioneer
-  pioneer = { :name => "Grace Hopper" }
+  pioneer = {:name => "Grace Hopper"}
 end
 
 def id_generator
-  id_generator = { :id => 2244856}
+  id_generator = {:id => 2244856}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = {key => value}
 end
 
 def read_from_hash(hash, key)
